e64bffc8e8a79aad7ab91f3b08b5fb338a341917
yxxndxxn <yxxndxxn@gmail.com>
2026-02-05 17:41:47 +0900

    [#11]; � fix: 카메라 config 수정

--- src/config/stages/stage3.js
+++ src/config/stages/stage3.js
@@ -6,8 +6,8 @@ export const STAGE3_CONFIG = {
     fov: 60.0,
     near: 0.1,
     far: 1000,
-    position: { x: -0.4, y: 6.1, z: 17.6 },
-    lookAt: { x: 0.3, y: -0.4, z: 0.5 },
+    position: { x: -0.4, y: 6.6, z: 19.5 },
+    lookAt: { x: 0.0, y: 1.2, z: 0.0 },
   },
   background: {
     color: 0x98d8aa, // 밝은 초원
