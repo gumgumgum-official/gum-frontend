9478ceb207903fe6b68d59e13970b3ffec16a358
kim nan seul <154604350+seulnan@users.noreply.github.com>
2026-05-22 16:37:29 +0900

    hotfix: 공항 문구 일부삭제

--- src/config/stages/stage6/stage6AirportAnnouncement.js
+++ src/config/stages/stage6/stage6AirportAnnouncement.js
@@ -19,16 +19,6 @@ export const STAGE6_AIRPORT_ANNOUNCEMENT_SUBTITLE_CUES = [
     text: "비행기로 이동하시기 전,\n공항 구석구석에 숨겨진 추억들을 마지막으로 한 번만 더 눈에 담아주세요.",
   },
   {
-    startSec: 19.1,
-    endSec: 22.6,
-    text: "혹시 두고 가시는 소중한 마음은 없으신가요?",
-  },
-  {
-    startSec: 23.6,
-    endSec: 28.6,
-    text: "포토 부스에서 찍은 '스티커 사진'은\n여러분의 가슴 속에 영원히 저장될 예정이니\n걱정 마세요!",
-  },
-  {
     startSec: 31.6,
     endSec: 37.1,
     text: "이곳에서의 기억이 여러분의 일상에\n말랑말랑한 껌딱지처럼 착! 달라붙어\n기분 좋은 힘이 되었으면 좋겠습니다.",
